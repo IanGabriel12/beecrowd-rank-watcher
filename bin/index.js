1d364119cd2e62217cf83a6d8a7131ef042300b7
Ian Gabriel <ianeigor12@gmail.com>
2024-04-14 11:21:40 -0300

    Default function

--- bin/index.js
+++ bin/index.js
@@ -5,15 +5,11 @@ const utils = require("./utils");
 const usage = "\n beewatcher <command> [args]";
 const options = yargs.usage(usage).help(true).argv;
 
-if(!options._[0]) {
-    yargs.showHelp();
-} else { 
-    switch(options._[0]) {
-        case "init":
-            utils.init();
-            break;
-        default:
-            console.log("No command detected");
-            break;
-    }
+switch(options._[0]) {
+    case "init":
+        utils.init();
+        break;
+    default:
+        yargs.showHelp()
+        break;
 }
